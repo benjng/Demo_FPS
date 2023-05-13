53f1f2ba75569d8a2e9a4cf2fc08927cf4a5829d
benjng <ppbenng@gmail.com>
2023-05-13 15:44:12 +0900

    Gravity, ground check implemented

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -6,14 +6,43 @@ public class CharacterMovement : MonoBehaviour
 {
     [SerializeField] private CharacterController controller;
     [SerializeField] private float speed = 10f;
-    // Update is called once per frame
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private Transform groundChecker;
+    [SerializeField] private float groundDistance = 0.4f;
+    [SerializeField] private LayerMask groundMask;
+
+    private Vector3 velocity;
+    private bool isGrounded;
+
     void Update()
     {
+        GroundedCheck();
+
         // Gather Keyboard inputs
         float xInput = Input.GetAxis("Horizontal");
         float zInput = Input.GetAxis("Vertical");
-        
+
+        MoveCharacter(xInput, zInput);
+        CalGravity();
+    }
+
+    void MoveCharacter(float xInput, float zInput){
+        // transform.right/forward: right/front local dir of the player, not world x/z axies
         Vector3 moveDir = transform.right * xInput + transform.forward * zInput;
         controller.Move(moveDir * speed * Time.deltaTime);
+    }
+
+    void CalGravity(){
+        // y = 0.5*g * t^2
+        velocity.y += gravity * (Time.deltaTime);
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void GroundedCheck(){
+        isGrounded = Physics.CheckSphere(groundChecker.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0){
+            velocity.y = -2f; // constantly forcing the player falls
+        }
     }
 }
